421146667c4f5127951d2a4c652d0e9ad7b2eb9c
iWoodys <santirefu1312@gmail.com>
2025-04-28 01:10:39 -0300

    FIREBASE

--- firebase.py
+++ firebase.py
@@ -1,10 +1,27 @@
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, db
+import os
+import json
 
-cred = credentials.Certificate("firebase-key.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
+# Cargar el JSON desde una variable de entorno
+firebase_key = os.getenv("FIREBASE_KEY_JSON")
 
-def get_server_loadouts(guild_id):
-    return db.collection('servers').document(str(guild_id)).collection('loadouts')
+# Convertir el string a un objeto JSON
+firebase_key_dict = json.loads(firebase_key)
 
+# Usar ese objeto para inicializar las credenciales
+cred = credentials.Certificate(firebase_key_dict)
+
+firebase_url = os.getenv('FIREBASE_URL')
+
+firebase_admin.initialize_app(cred, {
+    'databaseURL': firebase_url
+})
+
+def get_server_loadouts(server_id):
+    ref = db.reference(f'loadouts/{server_id}')
+    return ref.get()
+
+def save_server_loadouts(server_id, loadouts):
+    ref = db.reference(f'loadouts/{server_id}')
+    ref.set(loadouts)
