0bb9b9c66d2ed25afd24c023b421eb4424bc3e45
iWoodys <santirefu1312@gmail.com>
2025-05-01 02:33:28 -0300

    ACTUALIZACION

--- firebase.py
+++ firebase.py
@@ -1,14 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os
-import json
 
-# Obtener clave Firebase desde variable de entorno
-firebase_key_json = os.getenv("FIREBASE_KEY_JSON")
-cred = credentials.Certificate(json.loads(firebase_key_json))
-
-# Inicializar Firebase
-firebase_admin.initialize_app(cred)
+# Inicializar Firebase con el archivo JSON
+cred = credentials.Certificate("firebase-key.json")
+firebase_admin.initialize_app(cred)  # ✅ Solo una vez
 
 # Crear cliente Firestore
 db = firestore.client()
